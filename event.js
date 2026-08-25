c6ca68a57adc7797b78fcf2f1c253ebc019b49d5
DHalphen58 <0508split09@gmail.com>
2026-08-24 21:11:50 -0400

    Keep event pages content-free

--- event.js
+++ event.js
@@ -1,6 +1,3 @@
 const name = new URLSearchParams(location.search).get('name') || 'Event';
 document.title = name + ' | CubeLibrary';
 document.querySelector('#event-name').textContent = name;
-const youtube = query => 'https://www.youtube.com/results?search_query=' + encodeURIComponent(query);
-document.querySelector('#videos').href = youtube(name + ' tutorial');
-document.querySelector('#algorithms').href = youtube(name + ' algorithms');

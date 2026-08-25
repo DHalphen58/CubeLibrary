1c811bd9872d8ad24c9fd0fc895a6e23142ab131
DHalphen58 <0508split09@gmail.com>
2026-08-24 21:11:37 -0400

    Add Face Turning Octahedron event

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const events = [
   '2x2x2 Cube', '3x3x3 Cube', '4x4x4 Cube', '5x5x5–7x7x7 Cubes',
   '3x3x3 Blindfolded', '4x4x4 & 5x5x5 Blindfolded',
   '3x3x3 Fewest Moves', '3x3x3 One-Handed', 'Clock', 'Megaminx',
-  'Pyraminx', 'Skewb', 'Square-1', 'Multi-Blind'
+  'Pyraminx', 'Skewb', 'Square-1', 'Face Turning Octahedron', 'Multi-Blind'
 ];
 
 const grid = document.querySelector('#event-grid');

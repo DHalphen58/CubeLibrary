37fad86090038459c67035ab2987ec0a7b0164b0
DHalphen58 <0508split09@gmail.com>
2026-08-24 23:00:28 -0400

    Order events like the WCA event list

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const events = [
-  '2x2x2 Cube', '3x3x3 Cube', '4x4x4 Cube', '5x5x5–7x7x7 Cubes',
-  '3x3x3 Blindfolded', '4x4x4 & 5x5x5 Blindfolded',
-  '3x3x3 Fewest Moves', '3x3x3 One-Handed', 'Clock', 'Megaminx',
-  'Pyraminx', 'Skewb', 'Square-1', 'Face Turning Octahedron', 'Multi-Blind'
+  '3x3x3 Cube', '2x2x2 Cube', '4x4x4 Cube', '5x5x5–7x7x7 Cubes',
+  '3x3x3 Blindfolded', '3x3x3 Fewest Moves', '3x3x3 One-Handed',
+  'Clock', 'Megaminx', 'Pyraminx', 'Skewb', 'Square-1',
+  'Face Turning Octahedron', '4x4x4 & 5x5x5 Blindfolded', 'Multi-Blind'
 ];
 
 const grid = document.querySelector('#event-grid');
